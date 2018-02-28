f3e00914d965193399c0cf474b164fc3d97bc660
Cwander83 <chriswandermail@gmail.com>
2018-02-28 12:16:56 -0500

    2-28-18(fixing issues)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,5 @@
 /*--------------------------------------------------------------*/
 
-
-
-
-
 //start running functions
 //this function runs when user hits key
 
@@ -15,35 +11,21 @@ document.onkeyup = function (event) {
 };
 
 var myWord = getRandomWord();
-
 displayOnScreen();
-
 choiceWord();
 
 var wordChoice = "_";
-
 var letterChoice = [];
-
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "m", "n", "o", "p",
-    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-];
-
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    
 var wins = 0;
-
 var losses = 0;
-
 var guesses = 15;
-
-
 
 console.log(myWord);
 
-function choiceWord() {
-
-
-
-
-
+choiceWord = () => {
 
     document.getElementById("userText").textContent = myWord;
     console.log("hi");
@@ -53,35 +35,23 @@ function fillLetters() {
 
 }
 
-function displayOnScreen() {
+displayOnScreen = () => {
     var text = "";
     for (var i = 0; i < myWord.length; i++) {
         text += "_ ";
-
     }
-
     console.log(text);
-
     document.getElementById("userText").textContent = text;
-
-
-
 }
 
 var userText = document.getElementById("userText");
 
-
-
-
-
-function getRandomWord() {
+getRandomWord = () => {
     //start by listing out which words i want the gamer to have to find in hamgman
-
 
     var randomWordArray = ["YODA", "BOUNTYHUNTER", "JABBATHEHUT", "LUKE", "PRINCESSLEIA", "DARTHVADER", "EMPIRETROOPER", "CHEWBACCA"];
 
     // create a list of variables i think i will need to run the game
-
 
     var randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
     return randomWord;
